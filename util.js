b06e4afddf7ac8d91fbc88b37d21f6dcd5cfa62b
Samurais <hailiang.hl.wang@gmail.com>
2015-12-23 12:15:01 +0800

    kick off

---
+++ util.js
@@ -0,0 +1,44 @@
+/* format string value with arguments */
+String.prototype.format = String.prototype.f = function() {
+    var s = this,
+        i = arguments.length;
+
+    while (i--) {
+        s = s.replace(new RegExp('\\{' + i + '\\}', 'gm'), arguments[i]);
+    }
+    return s;
+};
+
+/* if a string ends with a given suffix */
+String.prototype.endsWith = function(suffix) {
+    return this.indexOf(suffix, this.length - suffix.length) !== -1;
+};
+
+/* if a string starts with a given prefix */
+String.prototype.startsWith = function(str) {
+    return this.indexOf(str) === 0;
+};
+
+window.isMobile = {
+    ua: (function() {
+        return navigator.userAgent.toLowerCase();
+    })(),
+    Wechat: function() {
+        return /micromessenger/i.test(this.ua);
+    },
+    Android: function() {
+        return /android/i.test(this.ua);
+    },
+    BlackBerry: function() {
+        return /blackberry/i.test(this.ua);
+    },
+    iOS: function() {
+        return /iphone|ipad|ipod/i.test(this.ua);
+    },
+    Windows: function() {
+        return /IEMobile/i.test(this.ua);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Windows());
+    }
+};
